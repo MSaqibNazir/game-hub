27e0c7ed8845bded54d4cfe3f8da36c55c84a1e9
= <=>
2023-06-13 21:24:32 +0500

    Filter Games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,43 @@
-import React from "react";
-import useGenre from "../hooks/useGenre";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
+import useGenre, { Genre } from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
-  const { genres } = useGenre();
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
+  const { data, isLoading, error } = useGenre();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
-    <ul>
-      {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY={"5px"}>
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              variant="link"
+              fontSize="lg">
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,41 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
-
-interface Genre {
+import useData from "./useData";
+export interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
 
-interface FetchGenreResponse {
-  count: number;
-  results: Genre[];
-}
-const useGenre = () => {
-  const [genres, setgenre] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchGenreResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setgenre(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenre = () => useData<Genre>("/genres");
 
 export default useGenre;
