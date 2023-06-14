f6d63a9cb1282372fbea30dabb70e17fa99a9087
= <=>
2023-06-14 20:56:13 +0500

    add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScrore from "./CriticScrore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -17,7 +18,10 @@ const GameCard = ({ game }: Props) => {
             <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
             <CriticScrore score={game.metacritic} />
           </HStack>
-          <Heading fontSize={"2xl"}>{game.name}</Heading>
+          <Heading fontSize={"2xl"}>
+            {game.name}
+            <Emoji rating={game.rating_top} />{" "}
+          </Heading>
         </CardBody>
       </Card>
     </>
